4e30288b85b992dd306c7df6712c7910c36116f2
SaranshSinha <31996102+SaranshSinha@users.noreply.github.com>
2019-11-26 00:30:51 +0530

    Update hello2.py

--- hello2.py
+++ hello2.py
@@ -1,21 +1,12 @@
 import sqlite3
-#li=[6,'saransh',21,'Gujarat',10000.0]
+li=[['pizza', 300],['cigaratte',300],['OYO',1000]]
 conn=sqlite3.connect("ExpenseTracker.db")
 print("opened database successfully!")
 try:
-    conn.execute('''CREATE TABLE IF NOT EXISTS Expense
+    conn.execute('''CREATE TABLE IF NOT EXISTS Budget
 
-    (username CHAR(10)  NOT NULL,
-
+    (
     item CHAR(20) NOT NULL,
-
-    tag TEXT NOT NULL,
-
-    type TEXT NOT NULL,
-
-    amount INT ,
-
-    edate DATE,
     lim INT
     );''')
 
@@ -23,27 +14,24 @@ except:
     print()
 
 print("Table created successfully")
-conn.execute("""INSERT INTO Expense
+#conn.execute("""INSERT INTO Expense
 
-VALUES ('saransh', 'pizza','food', 'expense',100, date('now'),0 );""")
-print("data inserted successfully!")
-#conn.execute("""INSERT INTO COMPANY VALUES(?,?,?,?,?);""",li)
+#VALUES ('saransh', 'pizza','food', 'expense',100, date('now'),0 );""")
+#print("data inserted successfully!")
+for i in  li:
+    conn.execute("""INSERT INTO Budget VALUES(?,?);""",i)
 
 conn.commit()
 print("now showing the data: ")
-cursor = conn.execute("SELECT * from Expense")
+cursor = conn.execute("SELECT * from Budget")
 
 for row in cursor:
 
-    print("NAME = ", row[0])
+    print("ITEM = ", row[0])
 
-    print("ITEM = ", row[1])
+    print("LIMIT = ", row[1])
+    print("------------")
 
-    print("CATEGORY = ", row[2])
 
-    print("TYPE = ", row[3])
-    print("AMOUNT = ", row[4])
-    print("DATE = ", row[5])
-    print("LIMIT = ", row[6])
 print("everything is working fine so far!")
 conn.close()
